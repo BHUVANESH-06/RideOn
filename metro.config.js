fafd8c7445a2d2a6e60ca70b494c9316010d6894
Bhuvanesh <bhuvanesh.g666@gmail.com>
2025-03-15 17:08:22 +0530

    added check image

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,11 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
-
+config.resolver.alias = {
+    "@/assets": "./assets",
+    "@/components": "./components",
+    "@/constants": "./constants",
+    "@/screens": "./screens",
+    "@/types":"./types",
+}
 module.exports = withNativeWind(config, { input: './global.css' })
